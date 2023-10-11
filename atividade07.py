65079f97906df2d63ac9819eb9bb30d7743750cc
Leofelixll <fleo16leo@gmail.com>
2023-10-11 12:41:44 -0300

    first commit

---
+++ atividade07.py
@@ -0,0 +1,6 @@
+# Digite um valor e veja quantos dolares voce poderá comprar: R$
+
+print("conversor de real para dólar ")
+real = float(input('Digite um valor em reais(R$): '))
+dolar = real*5
+print(f' você terá {dolar} dolares.')
